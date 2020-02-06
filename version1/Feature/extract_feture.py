cfaffd624a9b574aa39e1be69febfd5f4ff5c97b
591692335@qq.com <591692335@qq.com>
2020-02-06 17:35:08 +0800

    第二次修改：简单配置文件结构

--- version1/Feature/extract_feture.py
+++ version1/Feature/extract_feture.py
@@ -131,7 +131,8 @@ def item_id_feture(data,end_time,beforeoneday):
         beforethreeday = data[data['daystime']>=end_time-datetime.timedelta(days=7)]
         item_count_before_2 = pd.crosstab(beforethreeday.item_id,beforethreeday.behavior_type)
         
-    item_count_unq = data.groupby(by = ['item_id','behavior_type']).agg({"user_id":lambda x:x.nunique()});item_count_unq = item_count_unq.unstack()
+    item_count_unq = data.groupby(by = ['item_id','behavior_type']).agg({"user_id":lambda x:x.nunique()})
+    item_count_unq = item_count_unq.unstack()
     # beforeoneday = Data[Data['daystime'] == LabelDay-datetime.timedelta(days=1)]
     beforeonedayitem_count = pd.crosstab(beforeoneday.item_id,beforeoneday.behavior_type)
     countAverage = item_count/FEATURE_EXTRACTION_SLOT
@@ -221,7 +222,7 @@ def user_id_feture(data,end_time,beforeoneday):
     buyRate = buyRate.replace([np.inf, -np.inf], 0)
     buyRate_2 = buyRate_2.replace([np.inf, -np.inf], 0)
     buyRate_3 = buyRate_3.replace([np.inf, -np.inf], 0)
-
+    #tpt最大最小差
     long_online = pd.pivot_table(beforeoneday,index=['user_id'],values=['hours'],aggfunc=[np.min,np.max,np.ptp])
 
 
@@ -329,7 +330,6 @@ def user_cate_feture(data,end_time,beforeoneday):
 
 
 if __name__ == '__main__':
-#    pass
     result=[]
     for i in range(15):
         train_user_window1 = None
@@ -380,7 +380,7 @@ if __name__ == '__main__':
             LabelDay = datetime.datetime(2014,12,10,0,0,0)
         result.append(x)
     train_set = pd.concat(result,axis=0,ignore_index=True)
-#    train_set.to_csv('train_train_no_jiagou.csv',index=None)
+    train_set.to_csv(r'../result/train_train_no_jiagou.csv',index=None)
     ###############################################
     
     LabelDay=datetime.datetime(2014,12,18,0,0,0)
@@ -418,79 +418,4 @@ if __name__ == '__main__':
     test = pd.merge(test,add_user_cate_click,left_on = ['user_id','item_category'],right_index=True,how = 'left' )
     test = pd.merge(test,liveday,left_on = ['user_id'],right_index=True,how = 'left' )
     test = test.fillna(0)
-#    test.to_csv('test_test_no_jiagou.csv',index=None)
-#
-#    sys.exit()
-
-    ###############采样
-    train_set_1 = train_set[train_set['label']==1]
-    train_set_0 = train_set[train_set['label']==0]
-    new_train_set_0 = train_set_0.sample(len(train_set_1)*90)
-    train_set = pd.concat([train_set_1,new_train_set_0],axis=0)
-    ###############
-    train_y = train_set['label'].values
-    train_x = train_set.drop(['user_id', 'item_id','item_category', 'label'], axis=1).values
-    test_x = test.drop(['user_id', 'item_id','item_category'], axis=1).values   
-    num_round = 900
-    params = {'max_depth': 4, 'colsample_bytree': 0.8, 'subsample': 0.8, 'eta': 0.02, 'silent': 1,
-              'objective': 'binary:logistic','eval_metric ':'error', 'min_child_weight': 2.5,#'max_delta_step':10,'gamma':0.1,'scale_pos_weight':230/1,
-               'seed': 10}  #
-    plst = list(params.items())
-    dtrain = xgb.DMatrix(train_x, label=train_y)
-    dtest = xgb.DMatrix(test_x)
-    bst = xgb.train(plst, dtrain, num_round)
-    predicted_proba = bst.predict(dtest)
-    #print(predicted_proba)
-
-    predicted_proba = pd.DataFrame(predicted_proba)
-    predicted = pd.concat([test[['user_id', 'item_id']], predicted_proba], axis=1)
-    predicted.columns = ['user_id','item_id','prob']
-    #print(predicted)
-    predicted = predicted.sort_values('prob',  axis=0,ascending=False)
-    #print(predicted)
-#    predict1 = predicted.iloc[:650, [0, 1]]
-#    # 保存到文件
-#    predict1.to_csv("../result/10_30_2/650_1B80minchildweight1.8.csv", index=False)
-    
-    predict2 = predicted.iloc[:700, [0, 1]]
-    # 保存到文件
-    predict2.to_csv(BASE_DIR + "result/result.csv", index=False)
-    
-#    predict3 = predicted.iloc[:750, [0, 1]]
-#    # 保存到文件
-#    predict3.to_csv("../result/10_30_2/750_1B80minchildweight1.8.csv", index=False)
-    sys.exit()
-#    evaluate(predicted)
-
-
-
-
-    #####################################################################线下验证部分
-    reference = Data[Data['daystime'] == (LabelDay+datetime.timedelta(days=1))]
-    reference = reference[reference['behavior_type'] == 4]  # 购买的记录
-    reference = reference[['user_id', 'item_id']]  # 获取ui对
-    reference = reference.drop_duplicates(['user_id', 'item_id'])  # 去重
-    ui = predicted['user_id'] / predicted['item_id']
-
-    predicted=predicted[ui.duplicated() == False]
-
-    predicted_ui = predicted['user_id'] / predicted['item_id']
-    reference_ui = reference['user_id'] / reference['item_id']
-
-    is_in = predicted_ui.isin(reference_ui)
-    true_positive = predicted[is_in]
-
-    tp = len(true_positive)
-    predictedSetCount = len(predicted)
-    referenceSetCount = len(reference)
-
-    precision = tp / predictedSetCount
-    recall = tp / referenceSetCount
-
-    f_score = 2 * precision * recall / (precision + recall)
-
-    tp = recall * referenceSetCount
-    predictedSetCount = tp / precision
-
-    print('%.8f%% %.8f %.8f %.0f %.0f' %
-          (f_score * 100, precision, recall, tp, predictedSetCount))
+    test.to_csv(r'../result/test_test_no_jiagou.csv',index=None)
---
+++ version1/model/model_xgboost.py
@@ -0,0 +1,89 @@
+import xgboost as xgb
+import pandas as pd
+import sys
+import time
+import datetime
+
+BASE_DIR = r'G:\command_study\tianchi1/'
+Data = pd.read_csv(BASE_DIR + "DataSet/drop1112_sub_item.csv")
+Data['daystime'] = Data['days'].map(lambda x: time.strptime(x, "%Y-%m-%d")).map(lambda x: datetime.datetime(*x[:6]))
+
+
+if __name__ == '__main__':
+    print('---------------读取数据--------------')
+    train_set = pd.read_csv(r'../result/train_train_no_jiagou.csv')
+    test = pd.read_csv(r'../result/test_test_no_jiagou.csv')
+    train_set_1 = train_set[train_set['label'] == 1]
+    train_set_0 = train_set[train_set['label'] == 0]
+    # 抽取
+    print('---------------数据抽样--------------')
+    new_train_set_0 = train_set_0.sample(len(train_set_1) * 90)
+    train_set = pd.concat([train_set_1, new_train_set_0], axis=0)
+    ###############
+    train_y = train_set['label'].values
+    train_x = train_set.drop(['user_id', 'item_id', 'item_category', 'label'], axis=1).values
+    test_x = test.drop(['user_id', 'item_id', 'item_category'], axis=1).values
+    num_round = 900
+    params = {'max_depth': 4, 'colsample_bytree': 0.8, 'subsample': 0.8, 'eta': 0.02, 'silent': 1,
+              'objective': 'binary:logistic', 'eval_metric ': 'error', 'min_child_weight': 2.5,
+              # 'max_delta_step':10,'gamma':0.1,'scale_pos_weight':230/1,
+              'seed': 10}  #
+    plst = list(params.items())
+    dtrain = xgb.DMatrix(train_x, label=train_y)
+    dtest = xgb.DMatrix(test_x)
+    print('---------------训练模型--------------')
+    bst = xgb.train(plst, dtrain, num_round)
+    print('---------------数据测试--------------')
+    predicted_proba = bst.predict(dtest)
+    # print(predicted_proba)
+
+    predicted_proba = pd.DataFrame(predicted_proba)
+    predicted = pd.concat([test[['user_id', 'item_id']], predicted_proba], axis=1)
+    predicted.columns = ['user_id', 'item_id', 'prob']
+    # print(predicted)
+    predicted = predicted.sort_values('prob', axis=0, ascending=False)
+    # print(predicted)
+    #    predict1 = predicted.iloc[:650, [0, 1]]
+    #    # 保存到文件
+    #    predict1.to_csv("../result/10_30_2/650_1B80minchildweight1.8.csv", index=False)
+
+    predict2 = predicted.iloc[:700, [0, 1]]
+    # 保存到文件
+    predict2.to_csv("../result/tianchi_mobile_recommendation_predict.csv", index=False)
+
+    #    predict3 = predicted.iloc[:750, [0, 1]]
+    #    # 保存到文件
+    #    predict3.to_csv("../result/10_30_2/750_1B80minchildweight1.8.csv", index=False)
+    sys.exit()
+    #    evaluate(predicted)
+
+
+    #####################################################################线下验证部分
+    reference = Data[Data['daystime'] == (LabelDay + datetime.timedelta(days=1))]
+    reference = reference[reference['behavior_type'] == 4]  # 购买的记录
+    reference = reference[['user_id', 'item_id']]  # 获取ui对
+    reference = reference.drop_duplicates(['user_id', 'item_id'])  # 去重
+    ui = predicted['user_id'] / predicted['item_id']
+
+    predicted = predicted[ui.duplicated() == False]
+
+    predicted_ui = predicted['user_id'] / predicted['item_id']
+    reference_ui = reference['user_id'] / reference['item_id']
+
+    is_in = predicted_ui.isin(reference_ui)
+    true_positive = predicted[is_in]
+
+    tp = len(true_positive)
+    predictedSetCount = len(predicted)
+    referenceSetCount = len(reference)
+
+    precision = tp / predictedSetCount
+    recall = tp / referenceSetCount
+
+    f_score = 2 * precision * recall / (precision + recall)
+
+    tp = recall * referenceSetCount
+    predictedSetCount = tp / precision
+
+    print('%.8f%% %.8f %.8f %.0f %.0f' %
+          (f_score * 100, precision, recall, tp, predictedSetCount))
